da0f3e7da24a34d124c9bd06f9fc9456eccd55f2
Muralidhara Juluri <muralidhara.juluri@olacabs.com>
2017-03-08 09:34:20 +0530

    added a projects handler

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ hbs.registerPartials(__dirname + '/views/partial');
 app.set('view engine',hbs);
 app.use(express.static(__dirname + '/public'));
 
-app.use((req,res,next) => {
-	res.render('maintain.hbs',{
-		pageTitle : 'Under Maintenance'
-	});
-});
+// app.use((req,res,next) => {
+// 	res.render('maintain.hbs',{
+// 		pageTitle : 'Under Maintenance'
+// 	});
+// });
 
 hbs.registerHelper('getCurrentYear',() => {
 	return new Date().getFullYear();
@@ -32,6 +32,13 @@ app.get('/',(req,res) => {
 app.get('/about',(req,res) => {
 	res.render('about.hbs',{
 		pageTitle : 'About Page',
+		currentYear : new Date().getFullYear()
+	});
+});
+
+app.get('/projects',(req,res) => {
+	res.render('projects.hbs',{
+		pageTitle : 'Projects Page',
 		currentYear : new Date().getFullYear()
 	});
 });
